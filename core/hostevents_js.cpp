1eb290f6b850e5573fc8cbe9b1948d17ec111f23
Mihai Parparita <mihai@persistent.info>
2023-09-08 23:15:16 -0700

    Consume audio buffer and send to JS
    Turns out to be on the critical path to getting boot. If we don't
    pull data from the audio DMA channel it appears to fill up and the
    boot process hangs very early.
    
    We would have just had a no-op read, but might as well actually
    send the data to the JS side. We use a 1khz polling loop, which is
    perhaps too frequent, but it works for now.
    
    The 6100 guitar chord startup chime now plays, but there is some
    glitching after, and then a continous hum.
    
    Polling for input also turns out to be on the critical path,
    since we need to consume the input buffer to get the "audio context
    running" flag sent from the browser process to the worker. We don't
    actually do anything with the input data yet.

--- core/hostevents_js.cpp
+++ core/hostevents_js.cpp
@@ -21,13 +21,19 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <core/hostevents.h>
 #include <loguru.hpp>
+#include <emscripten.h>
 
 EventManager* EventManager::event_manager;
 bool g_swap_command_option = false;
 
 void EventManager::poll_events()
 {
-    // LOG_F(INFO, "EventManager::poll_events()");
+    int lock = EM_ASM_INT_V({ return workerApi.acquireInputLock(); });
+    if (!lock) {
+        return;
+    }
+    EM_ASM({ workerApi.releaseInputLock(); });
+
     // perform post-processing
     this->_post_signal.emit();
 }
