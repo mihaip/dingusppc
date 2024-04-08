1e05cb65a44c7e8bdc8e35458bef450ad68ba870
Mihai Parparita <mihai@persistent.info>
2024-04-08 11:49:30 -0500

    Inform the JS side of fatal errors
    It can display a more prominent error UI (and not go into to the
    debugger, which is not supported).

--- main_js.cpp
+++ main_js.cpp
@@ -1,8 +1,18 @@
 #include <main.h>
 #include <loguru.hpp>
+#include <emscripten.h>
+#include <iostream>
+#include <string>
 
 bool init() {
     LOG_F(INFO, "init()\n");
+
+    loguru::set_fatal_handler([](const loguru::Message& message) {
+        std::cerr << message.preamble << message.indentation << message.prefix << message.message << std::endl;
+        // The abort() call that follows this handler will not have a detailed message, so report it here.
+        std::string js_message = std::string(message.preamble) + message.prefix + message.message;
+        EM_ASM_({ workerApi.setAbortError(UTF8ToString($0)); }, js_message.c_str());
+    });
     return true;
 }
 
