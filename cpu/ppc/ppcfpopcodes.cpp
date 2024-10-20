d709db5a454a81b86b57c2e51f9802c206d5539a
Mihai Parparita <mihai@persistent.info>
2024-10-19 20:07:52 -0700

    Disable unsupported floating point exceptions in the Emscripten build
    Intentionally missing per emscripten-core/emscripten#11087

--- cpu/ppc/ppcfpopcodes.cpp
+++ cpu/ppc/ppcfpopcodes.cpp
@@ -128,7 +128,7 @@ static void fpresult_update(double set_result) {
         } else {
             ppc_state.fpscr |= FPCC_ZERO;
         }
-
+#ifndef EMSCRIPTEN
         if (std::fetestexcept(FE_OVERFLOW)) {
             ppc_state.fpscr |= (OX + FX);
         }
@@ -138,7 +138,7 @@ static void fpresult_update(double set_result) {
         if (std::fetestexcept(FE_DIVBYZERO)) {
             ppc_state.fpscr |= (ZX + FX);
         }
-
+#endif
         std::feclearexcept(FE_ALL_EXCEPT);
 
         if (std::isinf(set_result))
